131f346bea5c54b4d626abe3846288a92bb0dcf5
XUMO-97 <184026603@qq.com>
2018-10-24 17:25:41 +0800

    add test contract and readme file

---
+++ 上下班打卡签到DAPP项目/ClockInOut-test.sol
@@ -0,0 +1,69 @@
+pragma solidity ^0.4.16;
+
+//此处开始实现程序功能的主要代码即ClockInOut合约的代码
+contract ClockInOut {
+
+    //员工结构体
+    struct staff {
+        uint staffId; //定义员工号
+        string name; //定义员工名
+        bool clockIn; //是否上班打卡
+        bool clockOut; //是否下班签到
+    }
+
+    uint numStaffs; //统计员工数量
+    mapping (uint => staff) public staffs; //以键对的形式存储员工信息
+
+    //取得当前的时间
+    uint public timestamp = now;
+
+    //注意!!由于调用的时间戳为格林尼治时间,在国内使用时需要在getHour处加上8就是北京时间
+    function getHour(uint timestamp) public pure returns (uint8) {
+            return uint8((timestamp / 60 / 60) % 24 + 8);
+    }
+
+    function getMinute(uint timestamp) public pure returns (uint8) {
+            return uint8((timestamp / 60) % 60);
+    }
+
+    //构造函数
+    function ClockInOut() public {
+        numStaffs = 0;
+    }
+
+    //注册新增员工信息,需要传入员工名和员工号
+    function newStaff(uint staffId, string memory name) public {
+        //创建一个员工对象,并存储到staffs里面
+        staffs[staffId] = staff(staffId,name,false,true);
+        numStaffs = numStaffs + 1; //统计员工数
+    }
+
+    //通过输入员工号进行签到
+    function clockIn(uint id) public {
+        hour = getHour(timestamp);
+        if (staffs[id].clockOut == true && hour >= 16 && hour <= 17) {
+            staffs[id].clockIn = true;
+            staffs[id].clockOut = false;
+        }else{
+            revert();
+        }
+    }
+
+    //输入员工号进行打卡
+    function clockOut(uint id) public {
+        //通过id获取id对应的staff对象
+        hour = getHour(timestamp);
+        if (staffs[id].clockIn == true && hour >= 16 && hour <= 17) {
+            staffs[id].clockOut = true;
+            staffs[id].clockIn = false;
+        }else{
+            revert();
+        }
+    }
+
+    //调用函数读取统计的员工数量
+    function getNumStaffs() public view returns(uint) {
+        return numStaffs;
+    }
+
+}
---
+++ 上下班打卡签到DAPP项目/readme.md
@@ -0,0 +1,16 @@
+目前的程序直接获取最后区块创建的时间戳block.timestamp来计算签到打卡的时间,如果在公链或者测试链上部署可能会有十几分钟不等的误差,建议在创建区块时间间隔较短的私有链上部署测试.
+
+测试程序请使用ClockInOut-test.sol在http://remix.ethereum.org/上部署,建议选用0.4.20以下的solidity版本.
+
+尝试使用oraclize来获取最新时间戳,可以将误差缩小到几秒钟之内,使用oraclize自带的IDEhttp://dapps.oraclize.it可以在**JavaScript VM**模式下进行编译部署合约.
+
+但是使用该IDE操作并不能够向合约中发送eth来维持支付gas的费用,导致了只能够update一次即只能获取一次最新时间戳,如果向合约中发送eth会导致操作码错误的提示.
+
+搜索google和百度之后,目前并没有发现具体原因.
+
+所以尝试使用testrpc创建测试链环境,再通过remix和MetaMask来部署合约,就可以使用MetaMask钱包向合约地址发送eth来支付gas消耗.
+
+这种方法必须要使用ethereum-bridge来建立testrpc与oraclize的连接,否则无法引用OraclizeAPI的支持.
+
+目前在打在ethereum-bridge时遇到一些小问题,正在解决中...
+
